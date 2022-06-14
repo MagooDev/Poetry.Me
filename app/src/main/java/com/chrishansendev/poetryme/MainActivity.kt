8c53e08baef014d9e3fa4813737f042c53d9fab4
Christopher Hansen <hanschri@oregonstate.edu>
2022-06-13 18:34:57 -0700

    Initial commit

---
+++ app/src/main/java/com/chrishansendev/poetryme/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.chrishansendev.poetryme
+
+import android.animation.ObjectAnimator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.splashscreen.SplashScreen
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import java.util.Timer
+import kotlin.concurrent.schedule
+
+
+class MainActivity : AppCompatActivity() {
+
+    var contentHasLoaded = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startLoadingContent()
+        setupSplashScreen(splashScreen)
+    }
+
+    private fun startLoadingContent() {
+        // For this example, the Timer delay represents awaiting a response from a network call
+        Timer().schedule(3000){
+            contentHasLoaded = true
+        }
+    }
+
+    private fun setupSplashScreen(splashScreen: SplashScreen) {
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (contentHasLoaded) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else false
+                }
+            }
+        )
+
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val slideBack = ObjectAnimator.ofFloat(
+                splashScreenView.view,
+                View.TRANSLATION_X,
+                0f,
+                -splashScreenView.view.width.toFloat()
+            ).apply {
+                interpolator = DecelerateInterpolator()
+                duration = 800L
+                doOnEnd { splashScreenView.remove() }
+            }
+
+            slideBack.start()
+        }
+    }
+}
---
+++ app/src/main/java/com/chrishansendev/poetryme/SplashScreen.kt
@@ -0,0 +1,16 @@
+package com.chrishansendev.poetryme
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+    }
+}
